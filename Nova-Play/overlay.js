a1373d678b0c5f26d4c53012aaa9ea2513df8f8c
Michal92299 <michal.99967@gmail.com>
2025-07-11 12:53:44 +0200

    Delete Nova-Play directory

--- Nova-Play/overlay.js
+++
@@ -1,9 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-function performTransaction() {
-    alert('Transaction simulation: Sending 0.01 ETH (placeholder)');
-}
-
-function closeOverlay() {
-    ipcRenderer.send('close-overlay');
-}
